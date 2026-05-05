ac551e4ed6a4e191967455870a3814d9221f080c
Timson Folorunso <timsonfoli@gmail.com>
2026-05-05 13:29:46 +0100

    uncomment @CrossOrigin in ApiKeyController File

--- src/main/java/com/koolboks/creditProject/controller/ApiKeyController.java
+++ src/main/java/com/koolboks/creditProject/controller/ApiKeyController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/config")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 public class ApiKeyController {
 
     private final ApiKeyService apiKeyService;
